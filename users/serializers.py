5226ab7f6123ace51aceaac107bc45f308564ba9
bmamaxim <bma@gmail.com>
2024-06-28 22:47:04 +0500

    auth

--- users/serializers.py
+++ users/serializers.py
@@ -19,4 +19,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('first_name', 'email', 'avatar', 'habit')
+        fields = "__all__" # ('first_name', 'email', 'avatar', 'habit', 'password',)
