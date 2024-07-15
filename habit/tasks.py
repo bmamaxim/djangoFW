e8fea91c6ad5887fa9562c5b2caa630279bffda1
bmamaxim <bma@gmail.com>
2024-07-15 14:59:26 +0500

    fix

--- habit/tasks.py
+++ habit/tasks.py
@@ -18,21 +18,22 @@ def send_tg_message():
     """
 
     datetime_now = timezone.now()
-    print(datetime_now.time())
+
     habits = Habit.objects.all()
     for habit in habits:
-
-        last_try = habit.last_try
+        last_try_date = habit.last_try or datetime_now - datetime.timedelta(
+            days=999
+        )
         send_message = False
 
         if habit.periodicity == Habit.PERIOD_DAILY:
-            send_message = (datetime_now - last_try).days >= 1
+            send_message = (datetime_now - last_try_date).days >= 1
         elif habit.periodicity == Habit.EVERY_OTHER_DAYS:
-            send_message = (datetime_now - last_try).days >= 2
+            send_message = (datetime_now - last_try_date).days >= 2
         elif habit.periodicity == Habit.WEEKEND:
             send_message = (
                     datetime_now.weekday() in (6, 7)
-                    and (datetime_now - last_try).days >= 1
+                    and (datetime_now - last_try_date).days >= 1
             )
 
         if send_message and (datetime_now.time() >= habit.time):
